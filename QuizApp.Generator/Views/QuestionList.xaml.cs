795099508217e347733255f931beb4cf53fc2afe
byenio <marcel.bienko@gmail.com>
2025-04-30 10:43:25 +0200

    ui(86c39a5jv): add hover, fix scrollbar behavior

--- QuizApp.Generator/Views/QuestionList.xaml.cs
+++ QuizApp.Generator/Views/QuestionList.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace QuizApp.Generator.Views
 {
@@ -12,13 +14,35 @@ namespace QuizApp.Generator.Views
 
         private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (sender is ListViewItem item && item.DataContext is object question)
+            if (sender is not ListViewItem item || item.DataContext is not object question) return;
+            
+            if (DataContext is ViewModels.QuestionListViewModel vm)
             {
-                if (DataContext is ViewModels.QuestionListViewModel vm)
-                {
-                    vm.EditQuestionCommand.Execute(question);
-                }
+                vm.EditQuestionCommand.Execute(question);
             }
+        }
+        
+        private void QuestionListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollViewer = FindVisualChild<ScrollViewer>(QuestionListView);
+            double scrollAmount = e.Delta > 0 ? -1 : 1;
+            const double scrollMultiplier = 1;
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset + scrollAmount * scrollMultiplier);
+            e.Handled = true;
+        }
+        
+        private static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i);
+                if (child is T t)
+                    return t;
+                var childOfChild = FindVisualChild<T>(child);
+                
+                return childOfChild;
+            }
+            return null;
         }
     }
 }
